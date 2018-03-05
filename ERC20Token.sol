00f3cf9836825ee027352845e02a2ef171af0c54
Samuele Agostinelli <batonenbois@gmail.com>
2018-03-05 15:58:28 -0500

    added crowdsale

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -2,19 +2,7 @@ pragma solidity ^0.4.20;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
-
-
-interface ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
+import "./ERC20Interface.sol";
 
 
 
